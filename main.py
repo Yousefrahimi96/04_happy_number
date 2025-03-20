09582788e7996e1c15fc6aef398de96e920576f9
Yousefrahimi96 <yousefrahimi310@gmail.com>
2025-03-20 22:11:54 +0330

    remove try and except

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
 
 def happy_number():
-    current_number = input('inter your number: ')
+    current_number = input('Enter your number: ')
     seen_number = set()
-    
-    try:
-        current_number = int(input('Enter your number: '))
-    except ValueError:
-        print('Please enter a valid number.')
-        return
     
     while current_number != 1 and current_number not in seen_number:
         seen_number.add(current_number)
